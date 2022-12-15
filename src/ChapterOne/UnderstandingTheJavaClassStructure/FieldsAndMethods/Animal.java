fd228a84f06ba8458172c462228750e1cf7a8ab6
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2022-12-15 19:29:19 +0400

    Fields and Methods

---
+++ src/ChapterOne/UnderstandingTheJavaClassStructure/FieldsAndMethods/Animal.java
@@ -0,0 +1,12 @@
+package ChapterOne.UnderstandingTheJavaClassStructure.FieldsAndMethods;
+
+public class Animal {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName) {
+        name = newName;
+    }
+}
