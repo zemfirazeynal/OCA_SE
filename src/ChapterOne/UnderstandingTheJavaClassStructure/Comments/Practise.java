c194b5b0b85d52af1da512fe603da9d7e95bfb3f
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2022-12-16 16:13:35 +0400

    Comments

---
+++ src/ChapterOne/UnderstandingTheJavaClassStructure/Comments/Practise.java
@@ -0,0 +1,17 @@
+package ChapterOne.UnderstandingTheJavaClassStructure.Comments;
+
+public class Practise {
+    /*
+     * // anteater -- multiline comment
+     */
+    // bear -- basic single-line comment
+    // // cat -- single-line comment
+    // /* dog */ single-line comment
+    /* elephant */
+    /*
+     * /* ferret */
+
+    // line 10 -- basic multiple line comment
+    // lines 11,12 -- multiple line comment
+
+}
---
+++ src/ChapterOne/UnderstandingTheJavaClassStructure/Comments/TypesOfComments.java
@@ -0,0 +1,14 @@
+package ChapterOne.UnderstandingTheJavaClassStructure.Comments;
+
+public class TypesOfComments {
+    // comment until end of line
+
+    /* Multiple
+     * line comment
+     */
+
+    /**
+     * Javadoc multiple-line comment
+     * @author Jeanne and Scott
+     */
+}
